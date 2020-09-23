60a210d871b5453dbff3409beb06138bf2b93096
M.S. Sathwick Kiran <53941387+Sathwick17@users.noreply.github.com>
2020-09-23 15:52:55 +0530

    Update XORlinkedlist.cpp

--- Week1/XORlinkedlist.cpp
+++ Week1/XORlinkedlist.cpp
@@ -50,6 +50,28 @@ void insertEnd (Node **head, int data) {
     
 }  
   
+Node* deleteEnd(Node* head) {
+  if (!head) return NULL;
+  Node* prev = NULL, *curr = head, *next = XOR(prev, curr -> npx);
+
+  while (next) {
+    prev = curr;
+    curr = next;
+    next = XOR(prev, curr->npx);
+  }
+
+  prev -> npx = XOR(prev -> npx, curr);
+  delete curr;
+  return head;
+}
+
+Node* deleteBeg(Node* head) {
+  if (!head) return NULL;
+  Node* next = XOR(head -> npx, NULL);
+  next->npx = XOR(head, next -> npx);
+  delete head;
+  return next;
+}
 void printList (Node *head) {  
     Node *curr = head;  
     Node *prev = NULL;  
@@ -64,7 +86,7 @@ void printList (Node *head) {
         prev = curr;  
         curr = next;  
     }  
-}  4
+}  
   
 int main () {  
     Node *head = NULL; 
